bfa1277c0f54845e6ab53cef78a8f45466eb6e5f
asadesol <asad@esols.tech>
2026-07-16 05:55:09 +0500

    newest

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -4,7 +4,8 @@ export default function Footer() {
   return (
 
     <footer className="bg-white dark:bg-black border-t border-zinc-200 dark:border-zinc-800">
-    
+  <Script async data-cfasync="false" src="https://pl30350904.effectivecpmnetwork.com/b7f2477a65daaf396ef8b5a4c3c1153d/invoke.js"></Script>
+<div id="container-b7f2477a65daaf396ef8b5a4c3c1153d"></div>
 
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-center items-center h-16">
