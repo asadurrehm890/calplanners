c2c30e58e9aa5ffca5b41eee923bc391abfd05c6
asadesol <asad@esols.tech>
2026-08-21 10:41:59 +0500

    4584305834053450348509345

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "@/app/components/Header";
-import Footer from "@/app/components/Footer";
+import Header from "@/app/components/header";
+import Footer from "@/app/components/footer";
 
 
 const inter = Inter({
