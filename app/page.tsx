d28c9d0c5e53cd700897bfdda66f9a909de76984
asadesol <asad@esols.tech>
2026-08-21 11:47:15 +0500

    4584305834053450348509345

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,7 @@
-import Hero from "@/app/components/sections/Hero";
-import Services from "@/app/components/sections/Services";
-import Work from "@/app/components/sections/Work";
-import About from "@/app/components/sections/About";
-import Blog from "@/app/components/sections/Blog";
-import Contact from "@/app/components/sections/Contact";
-import TrustBadges from "@/app/components/sections/TrustBadges";
+
 
 export default function Home() {
   return (
-    <>
-      <Hero />
-      <TrustBadges />
-      <Services />
-      <Work />
-      <About />
-      <Blog />
-      <Contact />
-    </>
+   
   );
 }
